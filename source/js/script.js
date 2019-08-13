6760d183d18f5054534ad980601aa25f5a09987a
Konstantin <sithari41441@gmail.com>
2019-08-13 22:13:58 +0700

    Реализовано открытие/закрытие списков в Подвале

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,13 @@
 const requestCallButton = document.querySelector('.header-nav__button');
 const requestCallPopup = document.querySelector('.request-call-popup');
 const popupCloseButton = document.querySelector('[name = request-call-popup__close-button]');
+const footerNav = document.querySelector('.footer-nav');
+const footerNavHeader = document.querySelector('.footer-nav h2');
+const contacts = document.querySelector('.contacts');
+const contactsHeader = document.querySelector('.contacts h2');
 const escKeyCode = 27;
+
+// Открытие/закрытие попапа
 
 const clearPopupForm = () => {
   const popupNameInput = document.querySelector('[name = popup-form__name]');
@@ -13,7 +19,7 @@ const clearPopupForm = () => {
   popupQuestionInput.value = '';
 };
 
-const closePopup = () => {
+const closePopup = (elementName) => {
   requestCallPopup.classList.remove('request-call-popup--opened');
   requestCallPopup.classList.add('request-call-popup--closed');
   clearPopupForm();
@@ -41,5 +47,28 @@ window.addEventListener('keydown', (evt) => {
     if (requestCallPopup.classList.contains('request-call-popup--opened')) {
       closePopup();
     }
+  }
+});
+
+
+// Открытие/закрытие списков в подвале
+
+footerNavHeader.addEventListener('click', () => {
+  if (footerNav.classList.contains('footer-nav--closed')) {
+    footerNav.classList.remove('footer-nav--closed');
+    footerNav.classList.add('footer-nav--opened');
+  } else {
+    footerNav.classList.remove('footer-nav--opened');
+    footerNav.classList.add('footer-nav--closed');
+  }
+});
+
+contactsHeader.addEventListener('click', () => {
+  if (contacts.classList.contains('contacts--closed')) {
+    contacts.classList.remove('contacts--closed');
+    contacts.classList.add('contacts--opened');
+  } else {
+    contacts.classList.remove('contacts--opened');
+    contacts.classList.add('contacts--closed');
   }
 });
